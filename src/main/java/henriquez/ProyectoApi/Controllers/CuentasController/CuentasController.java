c0a7bfc27fe330f487642fa333b3fc36a91c9718
MC-Henriquez <ventas@mchenriquez.com>
2026-01-26 09:29:17 -0600

    Update CuentasController.java

--- src/main/java/henriquez/ProyectoApi/Controllers/CuentasController/CuentasController.java
+++ src/main/java/henriquez/ProyectoApi/Controllers/CuentasController/CuentasController.java
@@ -31,7 +31,7 @@ public class CuentasController {
             return ResponseEntity.badRequest().build();
         }
 
-        EstadoCuentaResponseDTO response =
+        EstadoCuenta response =
                 service.obtenerEstadoCuenta(idCliente, fechaInicio, fechaFin);
 
         if (response.getDocuments().isEmpty()) {
